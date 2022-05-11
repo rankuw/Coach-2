f3a093f21ab150718b8e15062e8fee7b42d9185d
Suraj Rawat <suraj.rawat@appinventiv.com>
2022-05-11 07:07:24 +0000

    start dev sh file.

---
+++ scripts/start_dev.sh
@@ -0,0 +1,14 @@
+sudo -i
+mv /home/ec2-user/coach_project_dev_backend_artifact.tar.gz /var/www/html/coach_project/backend/dev/
+cd /var/www/html/coach_project/backend/dev/
+tar -xzf coach_project_dev_backend_artifact.tar.gz
+rm -rf coach_project_dev_backend_artifact.tar.gz
+chown -R coach_user:coach_user /var/www/html/coach_project/backend/dev/
+pwd
+ls -la
+su coach_user
+cp env.dev .env
+ls -la
+cd /var/www/html/coach_project/backend/
+pm2 delete dev
+pm2 start dev
